2efe7f81a3d2057b96c17460a3a9f36d31322f56
henry-on-the-internet <narrator0120@gmail.com>
2022-07-26 23:06:27 +1000

    add backend folder for routers and controllers

---
+++ backend/controller/user_controller.go
@@ -0,0 +1,7 @@
+package users
+
+import "github/com/gin-gonic/gin"
+
+func Register(c *gin.Context){
+	var users users.User
+}
---
+++ backend/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/henry-on-the-internet/go-react-auth/backend/app"
+
+func main(){
+	app.StartApplication()
+}
